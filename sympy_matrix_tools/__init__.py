3a13b58f102ed59426c749807cc11495fa4af091
J. Rockford <jrf@mbn.nifty.com>
2022-06-19 17:00:09 +0900

    update: v0.2.0

--- sympy_matrix_tools/__init__.py
+++ sympy_matrix_tools/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.2.0' # Time-stamp: <2022-06-19T01:26:49Z>
+__version__ = '0.2.0' # Time-stamp: <2022-06-19T07:59:00Z>
+
+import sympy
+from packaging.version import parse as pv
 
 from .matrix_tools import *
 from .matrix_function import *
@@ -7,4 +10,5 @@ from .seq_tools import *
 from .matrix_concrete import *
 from .apply import *
 from .unif_tools import *
-from .matrix_symbol import *
+if pv(sympy.__version__) >= pv('1.10.1'):
+    from .matrix_symbol import *
